7564eb3d9c003eed47a17820f630a228925442ba
hopa332211 <49374617+hopa332211@users.noreply.github.com>
2019-04-08 14:35:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
 client.on('message', msg => {
   if (msg.content === '-help') {
-    msg.reply('mmkn ttfdl el Guorp dh fe tklm STAFF ely feh Atfdl https://www.facebook.com/groups/OneDropCF/.!');
+    msg.reply('يجب عليك تنزيل منشور داخل الجورب الفيس لينك https://www.facebook.com/groups/OneDropCF/.!');
   }
 });
 client.login(process.env.BOT_TOKEN);
