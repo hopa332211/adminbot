99dc7acb768f79754cee3cec450400b5bfe9a74b
hopa332211 <49374617+hopa332211@users.noreply.github.com>
2019-04-08 14:34:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,20 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content === 'hi') {
-    msg.reply('Hello!');
-  }
-});
-client.login(process.env.BOT_TOKEN);
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-client.on('message', msg => {
-  if (msg.content === 'Welcome') {
-    msg.reply('Welcome Pro!');
+  if (msg.content === '-help') {
+    msg.reply('mmkn ttfdl el Guorp dh fe tklm STAFF ely feh Atfdl https://www.facebook.com/groups/OneDropCF/.!');
   }
 });
 client.login(process.env.BOT_TOKEN);
