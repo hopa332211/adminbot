942919afc839886d5b45e41a5ff5a3f19bea82b2
hopa332211 <49374617+hopa332211@users.noreply.github.com>
2019-04-08 14:30:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,6 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
-
 client.on('message', msg => {
   if (msg.content === 'Welcome') {
     msg.reply('Welcome Pro!');
