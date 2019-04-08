57a6f27c048cda45974d5598defcfb518bedd98c
hopa332211 <49374617+hopa332211@users.noreply.github.com>
2019-04-08 14:03:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,5 +10,56 @@ client.on('message', msg => {
     msg.reply('Pong!');
   }
 });
-
 client.login(process.env.BOT_TOKEN);
+
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Hi') {
+    msg.reply('Hello!');
+  }
+});
+client.login(process.env.BOT_TOKEN);
+
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Welcome') {
+    msg.reply('Welcome Pro <3!');
+  }
+});
+client.login(process.env.BOT_TOKEN);
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === '-help') {
+    msg.reply('Mmkn tfdl tklm 7d mn el STAFF fe el Guorp https://www.facebook.com/groups/OneDropCF/');
+  }
+});
+client.login(process.env.BOT_TOKEN);
+
+
+
+
+
+
+
+
